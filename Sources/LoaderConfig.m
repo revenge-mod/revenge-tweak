501c161bbe189808d0973b2e0e770ff99b5ac18d
pylix <82711525+pylixonly@users.noreply.github.com>
2024-11-15 23:53:59 +0800

    Merge pull request #22 from castdrian/fix-loader-config
    fix: loader config oversight

--- Sources/LoaderConfig.m
+++ Sources/LoaderConfig.m
@@ -8,25 +8,25 @@
     self = [super init];
     if (self) {
         self.customLoadUrlEnabled = NO;
-        self.customLoadUrl = [NSURL URLWithString:@"http://localhost:4040/bunny.js"];
+        self.customLoadUrl = [NSURL URLWithString:@"http://localhost:4040/revenge.js"];
     }
     return self;
 }
 
 - (BOOL)loadConfig {
     NSURL *loaderConfigUrl = [getPyoncordDirectory() URLByAppendingPathComponent:@"loader.json"];
-    BunnyLog(@"Attempting to load config from: %@", loaderConfigUrl.path);
-    
+    RevengeLog(@"Attempting to load config from: %@", loaderConfigUrl.path);
+
     if ([[NSFileManager defaultManager] fileExistsAtPath:loaderConfigUrl.path]) {
         NSError *error = nil;
         NSData *data = [NSData dataWithContentsOfURL:loaderConfigUrl];
         NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-        
+
         if (error) {
-            BunnyLog(@"Error parsing loader config: %@", error);
+            RevengeLog(@"Error parsing loader config: %@", error);
             return NO;
         }
-        
+
         if (json) {
             NSDictionary *customLoadUrl = json[@"customLoadUrl"];
             if (customLoadUrl) {
@@ -36,15 +36,57 @@
                     self.customLoadUrl = [NSURL URLWithString:urlString];
                 }
             }
-            
-            BunnyLog(@"Loader config loaded - Custom URL %@: %@", 
+
+            RevengeLog(@"Loader config loaded - Custom URL %@: %@",
                     self.customLoadUrlEnabled ? @"enabled" : @"disabled",
                     self.customLoadUrl.absoluteString);
             return YES;
         }
     }
-    
-    BunnyLog(@"Using default loader config: %@", self.customLoadUrl.absoluteString);
+
+    RevengeLog(@"Using default loader config: %@", self.customLoadUrl.absoluteString);
+    return NO;
+}
+
++ (instancetype)defaultConfig {
+    LoaderConfig *config = [[LoaderConfig alloc] init];
+    config.customLoadUrlEnabled = NO;
+    config.customLoadUrl = [NSURL URLWithString:@"http://localhost:4040/revenge.js"];
+    return config;
+}
+
+- (BOOL)loadConfig {
+    NSURL *loaderConfigUrl = [getPyoncordDirectory() URLByAppendingPathComponent:@"loader.json"];
+    RevengeLog(@"Attempting to load config from: %@", loaderConfigUrl.path);
+
+    if ([[NSFileManager defaultManager] fileExistsAtPath:loaderConfigUrl.path]) {
+        NSError *error = nil;
+        NSData *data = [NSData dataWithContentsOfURL:loaderConfigUrl];
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+
+        if (error) {
+            RevengeLog(@"Error parsing loader config: %@", error);
+            return NO;
+        }
+
+        if (json) {
+            NSDictionary *customLoadUrl = json[@"customLoadUrl"];
+            if (customLoadUrl) {
+                self.customLoadUrlEnabled = [customLoadUrl[@"enabled"] boolValue];
+                NSString *urlString = customLoadUrl[@"url"];
+                if (urlString) {
+                    self.customLoadUrl = [NSURL URLWithString:urlString];
+                }
+            }
+
+            RevengeLog(@"Loader config loaded - Custom URL %@: %@",
+                    self.customLoadUrlEnabled ? @"enabled" : @"disabled",
+                    self.customLoadUrl.absoluteString);
+            return YES;
+        }
+    }
+
+    RevengeLog(@"Using default loader config: %@", self.customLoadUrl.absoluteString);
     return NO;
 }
 
